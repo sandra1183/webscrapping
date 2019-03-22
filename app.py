da12d3a5034ba7761744467a30beac7694e25b38
Sandra Regan <42319451+sandra1183@users.noreply.github.com>
2019-03-22 13:06:58 -0400

    add

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, jsonify, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+app = Flask(__name__)
+
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars")
+
+
+
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
+
+# Scrape Data and pull into Mongo DB
+@app.route('/scrape')
+def get():
+    mars = mongo.db.mars
+    marsdata = scrape_mars.scrape()
+    mars.update({}, marsdata, upsert=True)
+    return redirect("/", code=302)
+
+
+
+if __name__ == "__main__":
+    app.run()
